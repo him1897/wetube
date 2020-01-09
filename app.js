ee34f69954e97094b3e9334499bc6034573c8cea
him1897 <53065360+him1897@users.noreply.github.com>
2020-01-09 16:57:12 +0900

    noway

--- app.js
+++ app.js
@@ -3,27 +3,25 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-
+import { localsMiddleware } from "./middleware";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
 
-const handleHome = (req, res) => res.send("Hi from ass!");
-
-const handleProfile = (req, res) => res.send("You are on my Profile");
-
 //middlewares
+app.use(helmet());
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
-
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
-
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
